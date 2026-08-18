fd81fbd52e9cb4cc535a408f34132db5649fdb2a
Rodrigo Sundfeld <rodrigo.psundfeld@gmail.com>
2026-08-18 08:15:53 -0300

    Plate counter, and plate Logic done

--- Assets/_Scripts/Counters/CuttingCounter/CuttingCounter.cs
+++ Assets/_Scripts/Counters/CuttingCounter/CuttingCounter.cs
@@ -47,7 +47,14 @@ public class CuttingCounter : BaseCounter, IHasProgress
             //There is a kitchen Object here
             if (player.HasKitchenObject())
             {
-
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+                {
+                    //Player is holding a plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
+                    {
+                        GetKitchenObject().DestroySelf();
+                    }
+                }
             }
             else
             {
